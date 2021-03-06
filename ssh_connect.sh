ade88b8d347e8018214fada2093d0445e1cb35b3
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2021-03-06 11:00:43 -0300

    Fix key value and use ssh -i commmand

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -3,11 +3,11 @@
 key=''
 case $1 in
 	machine_1)
-		key='paht_to_key1.pem'
+		key='path_to_key1.pem'
 		hostname='hostname_1'
 		;;
 	machine_2)
-		key='paht_to_key2.pem'
+		key='path_to_key2.pem'
 		hostname='hostname_2'
 		;;
 	*)
@@ -16,7 +16,7 @@ esac
 
 if [ -n "$key" ]
 then
-	echo $key $hostname
+	ssh -i $key $hostname
 else
 	echo "Machine $1 does not exist!"
 fi
