24d1eab0cc5679c9f0be1d093397e4a24e32833e
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2021-03-06 10:51:49 -0300

    Add key and hostname to cases

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+key=''
 case $1 in
-machine_1) echo "Machine 1" ;;
-machine_2) echo "Machine 2" ;;
-*) echo "Machine unknown" ;;
+	machine_1)
+		key='paht_to_key1.pem'
+		hostname='hostname_1'
+		;;
+	machine_2)
+		key='paht_to_key2.pem'
+		hostname='hostname_2'
+		;;
+	*)
+		;;
 esac
+
+if [ -n "$key" ]
+then
+	echo $key $hostname
+else
+	echo "Machine $1 does not exist!"
+fi
+
